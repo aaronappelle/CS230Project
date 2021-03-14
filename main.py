78c277331c1afe7e90cb66f21fa228c29e8b750d
Aaron Appelle <aaron.appelle@gmail.com>
2021-03-14 13:34:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -77,15 +77,15 @@ def main():
     #%% Split Data
     
     # Shuffle and shorten (for speed, for now)
-    # indices = np.random.randint(0,20000,(64,))
-    # X_train = X_train.take(indices,axis=0)
-    # y_train = y_train.take(indices,axis=0)
-    # indices = np.random.randint(0,2500,(64,))
-    # X_val = X_test.take(indices,axis=0)
-    # y_val = y_test.take(indices,axis=0)
-    # if args.semisupervised:
-    #     indices = np.random.randint(0,4000,(64,))
-    #     X_train_unlabeled = X_train_unlabeled.take(indices,axis=0)
+    indices = np.random.randint(0,20000,(64,))
+    X_train = X_train.take(indices,axis=0)
+    y_train = y_train.take(indices,axis=0)
+    indices = np.random.randint(0,2500,(64,))
+    X_val = X_test.take(indices,axis=0)
+    y_val = y_test.take(indices,axis=0)
+    if args.semisupervised:
+        indices = np.random.randint(0,4000,(64,))
+        X_train_unlabeled = X_train_unlabeled.take(indices,axis=0)
     
     # # Plot random test image
     # array_to_img(X_train[np.random.randint(len(X_train))][:,:,::-1]) # images in BGR!
